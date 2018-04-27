06bd07089e2178bb363a491f1d18e5b306c3391c
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-04-27 10:49:14 -0300

    commenting changes

--- defaultFunc/createEKG.m
+++ defaultFunc/createEKG.m
@@ -5,22 +5,22 @@ function [] = createEKG(setFiles, setPath, chann1, chann2)
 %       will looked for them within setpath.
 %       *setPath: path to folder containing set files.
 %       *chann1 & chann2: number indicating chann number.
+% 
+%       e.g.
+%       createEKG(setFiles, setPath, chann1, chann2)
 
-
+% if no set files received, look for them.
 if isempty(setFiles)
     setFile = dir([setPath '/*.set']);
     setFile = {setFile.name};
 end
 
+% loop through set files, load dataset, create ekg channel, save dataset.
 for i = 1:size(setFile, 2)
     currSet = setFile(i);
     
     tempEEG = pop_loadset('filename',char(currSet), 'filepath', char(setPath));    
     tempEEG = pop_eegchanoperator(tempEEG, {[['ch' int2str(tempEEG .nbchan + 1)] ' = ch' num2str(chann1) ' - ch' num2str(chann2) ' label EKG']} , 'ErrorMsg', 'popup', 'Warning', 'on');
     pop_saveset(tempEEG  , 'filename', char(currSet),'filepath', char(setPath));
-    
-    
 end
-
-
 end
